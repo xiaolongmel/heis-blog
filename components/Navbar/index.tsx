d7b6788efdda766294f15ee5aa5f655c24ee7bf4
xux <12345678>
2022-12-31 23:16:32 +0800

    chore: 安装scss,顶部导航初始化dom结构

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,7 +1,21 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
+import { navs } from './config';
+import styles from './index.module.scss';
 
 const Navbar: NextPage = () => {
-  return <div>页头</div>;
+  return (
+    <div className={styles.navbar}>
+      <section className={styles.logArea}>BLOG-c</section>
+      <section className={styles.linkArea}>
+        {navs?.map((nav) => (
+          <Link key={nav?.label} href={nav?.value}>
+            {nav?.label}
+          </Link>
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default Navbar;
