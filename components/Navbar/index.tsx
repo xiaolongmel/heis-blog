f58ff250fa391fbb362af1573479bb9102b4d687
xux <12345678>
2022-12-31 23:40:11 +0800

    chore: 页头页脚美化样式

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,15 +1,21 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { navs } from './config';
 import styles from './index.module.scss';
 
 const Navbar: NextPage = () => {
+  const { pathname } = useRouter();
   return (
     <div className={styles.navbar}>
-      <section className={styles.logArea}>BLOG-c</section>
+      <section className={styles.logoArea}>BLOG-C</section>
       <section className={styles.linkArea}>
         {navs?.map((nav) => (
-          <Link key={nav?.label} href={nav?.value}>
+          <Link
+            key={nav?.label}
+            href={nav?.value}
+            className={pathname === nav?.value ? styles.active : ''}
+          >
             {nav?.label}
           </Link>
         ))}
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,12 +1,13 @@
 import { NextPage } from 'next';
 import Footer from 'components/Footer';
 import Navbar from 'components/Navbar';
+import styles from './index.module.scss';
 
 const Layout: NextPage = ({ children }) => {
   return (
     <div>
       <Navbar />
-      <main>{children}</main>
+      <main className={styles.content}>{children}</main>
       <Footer />
     </div>
   );
