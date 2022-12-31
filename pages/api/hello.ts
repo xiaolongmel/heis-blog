55e78db72646df24f5d8b28517c07e269d475a2e
xux <12345678>
2022-12-31 21:22:33 +0800

    feat: 安装mockjs

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { mock } from 'mockjs';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+  name: string,
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    mock({
+      // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+      'list|1-10': [
+        {
+          // 属性 id 是一个自增数，起始值为 1，每次增 1
+          'id|+1': 1,
+        },
+      ],
+    })
+  );
 }
